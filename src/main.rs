42dbf30b5698fbd6633b91759a1a8d92b18a918c
Nicolas Mazzon <nicolasmazzon549@gmail.com>
2024-03-03 11:38:37 +0100

    Merge windows.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,63 @@ use eframe::{egui, Error};
 use egui::{Color32, FontData, FontFamily, FontId, RichText, TextStyle};
 use std::collections::BTreeMap;
 
+fn show_columns<R>(
+    ui: &mut egui::Ui,
+    split: f32,
+    add_contents: impl FnOnce(&mut egui::Ui, &mut egui::Ui) -> R,
+) -> R {
+    debug_assert!(split >= 0.0 && split <= 1.0);
+    let spacing = ui.spacing().item_spacing.x;
+    let total_spacing = spacing * (2 as f32 - 1.0);
+    let column_width = ui.available_width() - total_spacing;
+    let top_left = ui.cursor().min;
+
+    let (mut left, mut right) = {
+        let lpos = top_left;
+        let rpos = top_left + egui::vec2(split * (column_width + spacing), 0.0);
+
+        let lrect = egui::Rect::from_min_max(
+            lpos,
+            egui::pos2(
+                lpos.x + column_width * split,
+                ui.max_rect().right_bottom().y,
+            ),
+        );
+        let rrect = egui::Rect::from_min_max(
+            rpos,
+            egui::pos2(
+                rpos.x + column_width * (1.0 - split),
+                ui.max_rect().right_bottom().y,
+            ),
+        );
+
+        let mut lcolumn_ui =
+            ui.child_ui(lrect, egui::Layout::top_down_justified(egui::Align::LEFT));
+        let mut rcolumn_ui =
+            ui.child_ui(rrect, egui::Layout::top_down_justified(egui::Align::LEFT));
+        lcolumn_ui.set_width(column_width * split);
+        rcolumn_ui.set_width(column_width * (1.0 - split));
+        (lcolumn_ui, rcolumn_ui)
+    };
+
+    let result = add_contents(&mut left, &mut right);
+
+    let mut max_column_width = column_width;
+    let mut max_height = 0.0;
+    for column in &[left, right] {
+        max_column_width = max_column_width.max(column.min_rect().width());
+        max_height = column.min_size().y.max(max_height);
+    }
+
+    // make sure we fit everything next frame
+    let total_required_width = total_spacing + max_column_width * 2.0;
+
+    let size = egui::vec2(ui.available_width().max(total_required_width), max_height);
+    ui.advance_cursor_after_rect(egui::Rect::from_min_size(top_left, size));
+    result
+}
+
 struct App {
-    /// Whether or not to show the game.
-    show_viewport: bool,
     /// How many steps each simulation run does
     step_size: usize,
     /// Game being played.
@@ -33,7 +87,6 @@ impl App {
         cc.egui_ctx.style_mut(|s| s.text_styles = text_styles);
 
         Self {
-            show_viewport: false,
             game,
             step_size: 1,
         }
@@ -115,16 +168,7 @@ impl App {
         }
     }
 
-    fn show_viewport(&mut self, ui: &mut egui::Ui) {
-        self.show_grid(ui);
-
-        if ui.input(|i| i.viewport().close_requested()) {
-            // Tell parent viewport that we should not show next frame.
-            self.show_viewport = false;
-        }
-    }
-
-    fn show(&mut self, ui: &mut egui::Ui) {
+    fn show_left(&mut self, ui: &mut egui::Ui) {
         let text = RichText::new(format!("Step size {}", self.step_size)).size(16.0);
         let slider = egui::widgets::Slider::new(&mut self.step_size, 1..=20)
             .text(text)
@@ -145,32 +189,16 @@ impl App {
             self.game = Tournament::from(100, prisoners_dillemma_rules);
         }
 
-        if ui.button("Show simulation").clicked() {
-            self.show_viewport = true;
-        }
-
-        ui.with_layout(egui::Layout::bottom_up(egui::Align::RIGHT), |ui| {
-            ui.add(egui::Image::new(egui::include_image!("../felix.png")).max_width(200.0));
+        ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+            ui.add(egui::Image::new(egui::include_image!("../felix.png")));
         });
+    }
 
-        if self.show_viewport {
-            ui.ctx().show_viewport_immediate(
-                egui::ViewportId::from_hash_of("grid"),
-                egui::ViewportBuilder::default()
-                    .with_maximize_button(false)
-                    .with_title("Grid")
-                    .with_inner_size([800.0, 800.0])
-                    .with_min_inner_size([100.0, 100.0]),
-                |ctx, class| {
-                    assert!(
-                        class == egui::ViewportClass::Immediate,
-                        "This egui backend doesn't support multiple viewports"
-                    );
-
-                    egui::CentralPanel::default().show(ctx, |ui| self.show_viewport(ui));
-                },
-            );
-        }
+    fn show(&mut self, ui: &mut egui::Ui) {
+        show_columns(ui, 0.2, |lui, rui| {
+            self.show_left(lui);
+            self.show_grid(rui);
+        });
     }
 }
 
